42efb223d86d1de515eb3a18c018ec899e33eb9d
sheldonchiu <sheldon.chiu1998@gmail.com>
2023-07-02 22:41:22 +0800

    clean up code

--- rxconfig.py
+++ rxconfig.py
@@ -7,4 +7,20 @@ config = PaperspaceaiaioConfig(
     app_name="app",
     db_url="sqlite:///reflex.db",
     env=rx.Env.DEV,
+    tailwind={
+        "darkMode": 'class',
+        "theme": {
+            "colors":{
+            "gray1": "#F5F7F5",
+            "gray2": "#7885A7",
+            "blue1": "#243863",
+            "pink1": "#AC435B",
+            "dark1": "#1D131E",
+            },
+            "extend": {},
+        },
+        # "plugins": [
+        #     "@flowbite/plugin"
+        # ],
+    }
 )
